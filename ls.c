46b2b5b6a6f2e7542611092e04214224528db679
udayaan <nathudayaan@gmail.com>
2020-02-13 02:45:15 +0530

    ok

--- ls.c
+++ ls.c
@@ -1,10 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/xattr.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include "acl.h"
 #include<dirent.h>
-#include<stdlib.h>
+#include <pwd.h>
+#include <grp.h>
 
 int main(int argc, char  *argv[])
 {
-    struct dirent *de;  // Pointer for directory entry 
+    struct dirent *de; 
   
     DIR *dr = opendir(argv[1]); 
   
@@ -13,13 +22,31 @@ int main(int argc, char  *argv[])
         printf("Could not open current directory" ); 
         exit(EXIT_FAILURE); 
     } 
-  
-    // Refer http://pubs.opengroup.org/onlinepubs/7990989775/xsh/readdir.html 
-    // for readdir() 
-    while ((de = readdir(dr)) != NULL) 
-            printf("%s\n", de->d_name); 
-  
-    
+
+    while ((de = readdir(dr)) != NULL) {
+        if(strcmp(de->d_name,"..")!=0 && strcmp(de->d_name,".")!=0) {
+            struct stat filestat;
+
+            if(stat(de->d_name,&filestat)!=0) {
+                printf("%s\n",strerror(errno));
+                exit(EXIT_FAILURE);
+            }
+
+            int size = filestat.st_size;
+            struct passwd* user; 
+            if((user=getpwuid(filestat.st_uid))==NULL){
+                exit(EXIT_FAILURE);
+            }
+            
+            struct group* grp; 
+            if((grp=getgrgid(filestat.st_gid))==NULL){
+                exit(EXIT_FAILURE);
+            }
+            printf("owner:%s  group:%s %d   %s   %s \n", user->pw_name, grp->gr_name, size, de->d_name, 
+            S_ISDIR(filestat.st_mode)?"directory":"file"); 
+        }
+    }
+
     closedir(dr);  
     
     exit(EXIT_SUCCESS);
