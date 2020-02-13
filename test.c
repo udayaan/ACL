e9bab70b665b9057bb9a18b949a9f828508d9236
udayaan <nathudayaan@gmail.com>
2020-02-13 19:04:14 +0530

    ok

--- test.c
+++ test.c
@@ -6,38 +6,55 @@
 #include<sys/xattr.h>
 #include<errno.h>
 #include"./acl.h"
+#include<grp.h>
+#include <limits.h>
 
 int main(int argc, char  *argv[])
 {
-    char* path = "./testfile";
-    char* name = "user.test";
-    char* value = "testtest";
-    char buf[0];
-    if(setxattr(path,name,value,8,0)!=0) {
-        printf("1 %s\n",strerror(errno));
-        exit(1);
-    }
-    int size =  getxattr(path,name,&buf,0);
-    printf("%d\n",size);
-    char buf2[size+1];
-    getxattr(path,name,&buf2,size);
-    buf2[size] = '\0';
-    printf("%s\n",buf2);
-
-    // struct acl* meta = (struct acl*)malloc(sizeof(struct  acl));
-    // meta->acl = argv;
-    // meta->acllen = argc;
-    // char** ag = meta->acl;
-    // for (int i=0;i<argc;++i) {
-    //     printf("%s\n",ag[i]);
+    // char* path = "./testfile";
+    // char* name = "user.test";
+    // char* value = "testtest";
+    // char buf[0];
+    // if(setxattr(path,name,value,8,0)!=0) {
+    //     printf("1 %s\n",strerror(errno));
+    //     exit(1);
     // }
-    // printf("%li\n",strlen(ag[0]));
+    // int size =  getxattr(path,name,&buf,0);
+    // printf("%d\n",size);
+    // char buf2[size+1];
+    // getxattr(path,name,&buf2,size);
+    // buf2[size] = '\0';
+    // printf("%s\n",buf2);
 
-    // char* aclbuf = (char*)malloc(strlen(ag[0])*sizeof(char));
-    // memcpy(aclbuf,ag[0],strlen(ag[0])*sizeof(char));
+    // // struct acl* meta = (struct acl*)malloc(sizeof(struct  acl));
+    // // meta->acl = argv;
+    // // meta->acllen = argc;
+    // // char** ag = meta->acl;
+    // // for (int i=0;i<argc;++i) {
+    // //     printf("%s\n",ag[i]);
+    // // }
+    // // printf("%li\n",strlen(ag[0]));
+
+    // // char* aclbuf = (char*)malloc(strlen(ag[0])*sizeof(char));
+    // // memcpy(aclbuf,ag[0],strlen(ag[0])*sizeof(char));
     
-    // for (int i=0;i<strlen(ag[0]);++i) {
-    //     printf("%c\n",aclbuf[i]);
-    // }
+    // // for (int i=0;i<strlen(ag[0]);++i) {
+    // //     printf("%c\n",aclbuf[i]);
+    // // }
+
+    int ngrps=1000;
+    int* grps=(int*)malloc(ngrps*sizeof(gid_t));
+    getgrouplist(getlogin(),getegid(),grps,&ngrps);
+
+    printf("%d\n",ngrps);
+    struct group* gr;
+    for (int j = 0; j < ngrps; j++) {
+        printf("%d", grps[j]);
+        gr = getgrgid(grps[j]);
+        if (gr != NULL)
+            printf(" (%s)", gr->gr_name);
+        printf("\n");
+    }
+    printf("\n");
     return 0;
 }
